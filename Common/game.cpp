fac42dbf34646845be6af9524d329d137dbe362c
Demi Obenour <demiobenour@gmail.com>
2018-04-17 16:49:09 -0400

    Compressed ROMs are 33554432 bytes, not 67108864.

--- Common/game.cpp
+++ Common/game.cpp
@@ -10,6 +10,7 @@
 namespace OoT_Randomizer {
 using Decoder::loadAndDecodeFile;
 namespace Common {
+constexpr size_t COMPRESSED_GAME_SIZE = GAME_SIZE / 2;
 
 void randomize(std::vector<unsigned char> &data, bool randomize_chests, bool randomize_colors, std::uint64_t seed) {
     if (randomize_chests) {
@@ -22,8 +23,8 @@ void randomize(std::vector<unsigned char> &data, bool randomize_chests, bool ran
 }
 
 QString Execute(QString const &in_file, QString const &out_file, bool randomize_chests, bool randomize_colors, std::uint64_t seed, std::function<void(QString, int)> *cb) {
-    auto data = std::vector<unsigned char>(GAME_SIZE);
-    auto compressed_data = std::vector<unsigned char>(GAME_SIZE);
+    auto data = std::vector<unsigned char>(COMPRESSED_GAME_SIZE);
+    auto compressed_data = std::vector<unsigned char>(COMPRESSED_GAME_SIZE);
     static_assert(GAME_SIZE == 1 << 26, "GAME_SIZE must equal 1 << 26");
     static_assert(alignof (uint32_t) <= 4, "uint32_t must have an alignment <= 4");
     static_assert(sizeof (uint32_t) == 4, "uint32_t must have size 4");
@@ -32,8 +33,8 @@ QString Execute(QString const &in_file, QString const &out_file, bool randomize_
         if (!input_file.open(QIODevice::ReadOnly)) {
             return QStringLiteral("Cannot open input file %1: %2").arg(in_file, input_file.errorString());
         }
-        for (std::size_t i = 0, new_max{0}; i < GAME_SIZE; i = new_max) {
-            new_max = std::min(std::size_t{i + (1UL << 14)}, GAME_SIZE);
+        for (std::size_t i = 0, new_max{0}; i < COMPRESSED_GAME_SIZE; i = new_max) {
+            new_max = std::min(std::size_t{i + (1UL << 14)}, COMPRESSED_GAME_SIZE);
             auto bytes_to_read = qint64(new_max - i);
             qint64 bytes_read = input_file.read(reinterpret_cast<char*>(compressed_data.data()) + i, bytes_to_read);
             if (input_file.error()) {
@@ -51,7 +52,7 @@ QString Execute(QString const &in_file, QString const &out_file, bool randomize_
             }
 
             if (cb) {
-                int size = int(qulonglong(new_max)*100ULL/GAME_SIZE);
+                int size = int(qulonglong(new_max)*100ULL/COMPRESSED_GAME_SIZE);
                 cb->operator()(QStringLiteral("Reading input file\u2026 %1 complete").arg(size), size);
             }
         }
